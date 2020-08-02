39ba44e7bf919942d486b07fdc2d9c5d0e0c1057
Manish sahu <xrcwrn@gmail.com>
2020-08-02 16:29:23 +0530

    Update MainClass.java

--- JDBCOperations/src/ebhor/start/MainClass.java
+++ JDBCOperations/src/ebhor/start/MainClass.java
@@ -1,12 +1,11 @@
-/*
- * main method to call action
- */
 package ebhor.start;
 
+import com.sun.jndi.url.dns.dnsURLContext;
 import ebhor.action.EmployeeAction;
 import ebhor.model.Employee;
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.util.Scanner;
 
 /**
  *
@@ -16,63 +15,75 @@ public class MainClass {
 
     EmployeeAction action = new EmployeeAction();
 
-    enum operation {
-        ADD, UPDATE, DELETE, FETCHBYID, FETCHBYEMAIL, FETCHBYMOBNO, FETCHBYNAME,
-        FETCHBYCITY, FETCHBYSALRANGE, FETCHBYDOB, FETCHBYDOJRANGE, FETCHALL
-    };
-
     public static void main(String[] args) {
+        String DbOperation = "EXIT(0), ADD(1), UPDATE(2), DELETE(3), FETCHBYID(4), FETCHBYEMAIL(5), FETCHBYMOBNO(6), FETCHBYNAME(7),"
+                + "FETCHBYCITY(8), FETCHBYSALRANGE(9), FETCHBYDOB(10), FETCHBYDOJRANGE(11), FETCHALL(12)";
+
         MainClass mainclass = new MainClass();
-        operation o = operation.ADD;
-        switch (o) {
-            case ADD:
-                mainclass.addEmployee();
-                mainclass.fetchAllEmployee();
-                break;
-            case UPDATE:
-                mainclass.updateEmployee();
-                mainclass.fetchAllEmployee();
-                break;
-            case DELETE:
-                mainclass.deleteEmployee();
-                break;
-            case FETCHBYID:
-                mainclass.fetchEmployeeById();
-                break;
-            case FETCHBYEMAIL:
-                mainclass.fetchEmployeeByEmail();
-                break;
-            case FETCHBYMOBNO:
-                mainclass.fetchEmployeeByMobileNo();
-                break;
-            case FETCHBYNAME:
-                mainclass.searchEmployeeByName();
-                break;
-            case FETCHBYCITY:
-                mainclass.fetchEmployeeByCity();
-                break;
-            case FETCHBYSALRANGE:
-                mainclass.fetchEmployeeBySalaryRange();
-                break;
-            case FETCHBYDOB:
-                mainclass.fetchEmployeeByDob();
-                break;
-            case FETCHBYDOJRANGE:
-                mainclass.fetchEmployeeByDOjRange();
-                break;
-            case FETCHALL:
-                mainclass.fetchAllEmployee();
-                break;
-        }
+        int value = 0;
+        do {
+            System.out.println("valid operations are");
+
+            System.out.println(DbOperation);
+            System.out.println("Enter valid operation number 0-12");
+            Scanner scanner = new Scanner(System.in);
+            value = scanner.nextInt();
+
+            switch (value) {
+                case 1:
+                    mainclass.addEmployee();
+                    mainclass.fetchAllEmployee();
+                    break;
+                case 2:
+                    mainclass.updateEmployee();
+                    mainclass.fetchAllEmployee();
+                    break;
+                case 3:
+                    mainclass.deleteEmployee();
+                    break;
+                case 4:
+                    mainclass.fetchEmployeeById();
+                    break;
+                case 5:
+                    mainclass.fetchEmployeeByEmail();
+                    break;
+                case 6:
+                    mainclass.fetchEmployeeByMobileNo();
+                    break;
+                case 7:
+                    mainclass.searchEmployeeByName();
+                    break;
+                case 8:
+                    mainclass.fetchEmployeeByCity();
+                    break;
+                case 9:
+                    mainclass.fetchEmployeeBySalaryRange();
+                    break;
+                case 10:
+                    mainclass.fetchEmployeeByDob();
+                    break;
+                case 11:
+                    mainclass.fetchEmployeeByDOjRange();
+                    break;
+                case 12:
+                    mainclass.fetchAllEmployee();
+                    break;
+                case 0:
+                    System.out.println("Exiting code");
+                    break;
+                default:
+                    System.out.println("Not a valid entry");
+            }
+        } while (value != 0);
 
     }
 
     public void addEmployee() {
-        Employee employee1 = new Employee("Ram", "Kumar", "Street no 5 sector 5", "456456122", "te@gmail.com", "Bhilai", "CEO", Date.valueOf("1988-03-22"), Date.valueOf("2015-05-05"), new BigDecimal(150000));
-        Employee employee2 = new Employee("Mohan", "Lal", "Near Super market", "2342424343", "mohallal@gmail.com", "Indore", "Manager", Date.valueOf("1988-05-20"), Date.valueOf("2015-05-15"), new BigDecimal(130000));
-        Employee employee3 = new Employee("Shyam", "Bajpayee", "Shanti Nagar", "3452342423", "bajpayee.shyam@gmail.com", "Pune", "Manager", Date.valueOf("1989-06-10"), Date.valueOf("2016-06-22"), new BigDecimal(130000));
-        Employee employee4 = new Employee("Rita", "Kapoor", "Near HAL Layout", "6538659801", "ritakapoor@gmail.com", "Bangalore", "Analyst", Date.valueOf("1989-06-12"), Date.valueOf("2016-06-06"), new BigDecimal(120000));
-        Employee employee5 = new Employee("Nita", "Rai", "Sundar Nagar", "8345354325", "nitarai@gmail.com", "Raipur", "Analyst", Date.valueOf("1988-03-22"), Date.valueOf("2015-05-05"), new BigDecimal(120000));
+        Employee employee1 = new Employee("Ram", "Kumar", "Street no 5 sector 5", "123456789", "te@gmail.com", "Bhilai", "CEO", Date.valueOf("1988-03-22"), Date.valueOf("2015-05-05"), new BigDecimal(150000));
+        Employee employee2 = new Employee("Mohan", "Lal", "Near Super market", "234567891", "mohallal@gmail.com", "Indore", "Manager", Date.valueOf("1988-05-20"), Date.valueOf("2015-05-15"), new BigDecimal(130000));
+        Employee employee3 = new Employee("Shyam", "Bajpayee", "Shanti Nagar", "345678912", "bajpayee.shyam@gmail.com", "Pune", "Manager", Date.valueOf("1989-06-10"), Date.valueOf("2016-06-22"), new BigDecimal(130000));
+        Employee employee4 = new Employee("Rita", "Kapoor", "Near HAL Layout", "456789123", "ritakapoor@gmail.com", "Bangalore", "Analyst", Date.valueOf("1989-06-12"), Date.valueOf("2016-06-06"), new BigDecimal(120000));
+        Employee employee5 = new Employee("Nita", "Rai", "Sundar Nagar", "567891234", "nitarai@gmail.com", "Raipur", "Analyst", Date.valueOf("1988-03-22"), Date.valueOf("2015-05-05"), new BigDecimal(120000));
         action.insert(employee1);
         action.insert(employee2);
         action.insert(employee3);
@@ -81,12 +92,12 @@ public class MainClass {
     }
 
     public void updateEmployee() {
-        Employee employee1 = new Employee(1l, "Ram", "Kumar", "Street no 5 sector 5", "4564563442", "test@gmail.com", "Bhilai", "CEO", Date.valueOf("1988-03-22"), Date.valueOf("2015-05-05"), new BigDecimal(150000));
+        Employee employee1 = new Employee(1l, "Rameshwar", "Kumar", "Street no 5 sector 6", "4564563442", "test@gmail.com", "Bhilai", "CEO", Date.valueOf("1988-03-22"), Date.valueOf("2015-05-05"), new BigDecimal(150000));
         action.update(employee1);
     }
 
     public void deleteEmployee() {
-        Employee employee6 = new Employee("Ramesh", "Kumar", "Sadar market", "345234234", "rameshkumar1@gmail.com", "Bhopal", "Manager", Date.valueOf("1989-12-20"), Date.valueOf("2015-05-15"), new BigDecimal(100000));
+        Employee employee6 = new Employee("Ramesh", "Kumar", "Sadar market", "345234237", "rameshkumar1@gmail.com", "Bhopal", "Manager", Date.valueOf("1989-12-20"), Date.valueOf("2015-05-15"), new BigDecimal(100000));
         action.insert(employee6);
         action.delete(6l);
     }
